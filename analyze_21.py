95583d33afb21ade255e2c5abf7eedd8bfc477f3
TibetanSandFox <pawel.zyblewski@pwr.edu.pl>
2020-01-25 21:57:38 +0100

    Analyze 21.

---
+++ analyze_21.py
@@ -0,0 +1,331 @@
+"""
+Analiza zależności od szumu
+"""
+import numpy as np
+from tabulate import tabulate
+import matplotlib.pyplot as plt
+from scipy.signal import medfilt
+import pandas as pd
+from math import pi
+from scipy.ndimage.filters import gaussian_filter1d
+
+from matplotlib import rcParams
+
+# Set plot params
+rcParams["font.family"] = "monospace"
+colors = [(0, 0, 0), (0, 0, 0.9), (0.9, 0, 0), (0, 0, 0), (0, 0, 0.9), (0.9, 0, 0)]
+ls = ["--", "--", "--", "-", "-", "-"]
+lw = [1, 1, 1, 1, 1, 1]
+
+methods = ["NON-KNORAU2", "ROS-KNORAU2", "B2-KNORAU2", "NON-KNORAE2", "ROS-KNORAE2", "B2-KNORAE2"]
+label_noises = [
+    "0.01",
+    "0.03",
+    "0.05"
+]
+ln = [a.replace('.','-') for a in label_noises]
+distributions = ["0.05", "0.10"]
+dist = [a.replace('.','-') for a in distributions]
+drifts = ["gradual", "incremental", "sudden"]
+metrics = ["bac", "gmean", "f1", "precision", "recall", "specificity"]
+clfs = ["GNB"]
+
+scores = np.load("scores_21.npy")
+
+# print(scores.shape)
+
+def plot_runs(
+    clfs, metrics, selected_scores, methods, mean_scores, dependency, what
+):
+    fig = plt.figure(figsize=(4, 4))
+    ax = plt.axes()
+    for z, (value, label, mean) in enumerate(
+        zip(selected_scores, methods, mean_scores)
+    ):
+        label += "\n{0:.3f}".format(mean)
+        val = gaussian_filter1d(value, sigma=1, mode="nearest")
+
+        # plt.plot(value, label=label, c=colors[z], ls=ls[z])
+
+        plt.plot(val, label=label, c=colors[z], ls=ls[z], lw=lw[z])
+
+    # box = ax.get_position()
+    # ax.set_position([box.x0, box.y0 + box.height * 0.1, box.width, box.height * 0.9])
+    ax.legend(
+        loc=8,
+        # bbox_to_anchor=(0.5, -0.1),
+        fancybox=False,
+        shadow=True,
+        ncol=3,
+        fontsize=8,
+        frameon=False,
+    )
+
+    plt.grid(ls=":", c=(0.7, 0.7, 0.7))
+    plt.xlim(0, 200)
+    axx = plt.gca()
+    axx.spines["right"].set_visible(False)
+    axx.spines["top"].set_visible(False)
+
+    plt.title(
+        "%s.\n%s" % (dependency[k][:-1], metrics[i]),
+        fontfamily="serif",
+        y=1.04,
+        fontsize=8,
+    )
+    plt.ylim(0.0, 1.0)
+    plt.xticks(fontfamily="serif")
+    plt.yticks(fontfamily="serif")
+    plt.ylabel("score", fontfamily="serif", fontsize=8)
+    plt.xlabel("chunks", fontfamily="serif", fontsize=8)
+    plt.tight_layout()
+    plt.savefig("plots/experiment21/runs/%s/%s_%s_%s" % (what, clfs[j], metrics[i], dependency[k]), bbox_inches='tight', dpi=250)
+    plt.close()
+
+def plot_radars(
+    methods, metrics, table, classifier_name, parameter_name, what
+):
+    """
+    Strach.
+    """
+    columns = ["group"] + methods
+    df = pd.DataFrame(columns=columns)
+    for i in range(len(table)):
+        df.loc[i] = table[i]
+    df = pd.DataFrame()
+    df["group"] = methods
+    for i in range(len(metrics)):
+        df[table[i][0]] = table[i][1:]
+    groups = list(df)[1:]
+    N = len(groups)
+
+    # nie ma nic wspolnego z plotem, zapisywanie do txt texa
+    # print(df.to_latex(index=False), file=open("tables/%s.tex" % (filename), "w"))
+
+    # What will be the angle of each axis in the plot? (we divide the plot / number of variable)
+    angles = [n / float(N) * 2 * pi for n in range(N)]
+    angles += angles[:1]
+
+    # Initialise the spider plot
+    ax = plt.subplot(111, polar=True)
+
+    # If you want the first axis to be on top:
+    ax.set_theta_offset(pi / 2)
+    ax.set_theta_direction(-1)
+
+    # No shitty border
+    ax.spines["polar"].set_visible(False)
+
+    # Draw one axe per variable + add labels labels yet
+    plt.xticks(angles, metrics)
+
+    # Adding plots
+    for i in range(len(methods)):
+        values = df.loc[i].drop("group").values.flatten().tolist()
+        values += values[:1]
+        values = [float(i) for i in values]
+        ax.plot(
+            angles, values, label=df.iloc[i, 0], c=colors[i], ls=ls[i], lw=lw[i],
+        )
+
+    # Add legend
+    plt.legend(
+        loc="lower center",
+        ncol=3,
+        columnspacing=1,
+        frameon=False,
+        bbox_to_anchor=(0.5, -0.25),
+        fontsize=8,
+    )
+
+    # Add a grid
+    plt.grid(ls=":", c=(0.7, 0.7, 0.7))
+
+    # Add a title
+    plt.title("%s" % (parameter_name), size=11, y=1.08, fontfamily="serif")
+    plt.tight_layout()
+
+    # Draw labels
+    a = np.linspace(0, 1, 6)
+    plt.yticks(a[1:], ["%.1f" % f for f in a[1:]], fontsize=6, rotation=90)
+    plt.ylim(0.0, 1.0)
+    plt.gcf().set_size_inches(4, 4)
+    plt.gcf().canvas.draw()
+    angles = np.rad2deg(angles)
+
+    ax.set_rlabel_position((angles[0] + angles[1]) / 2)
+
+    har = [(a >= 90) * (a <= 270) for a in angles]
+
+    for z, (label, angle) in enumerate(zip(ax.get_xticklabels(), angles)):
+        x, y = label.get_position()
+        print(label, angle)
+        lab = ax.text(
+            x, y, label.get_text(), transform=label.get_transform(), fontsize=6,
+        )
+        lab.set_rotation(angle)
+
+        if har[z]:
+            lab.set_rotation(180 - angle)
+        else:
+            lab.set_rotation(-angle)
+        lab.set_verticalalignment("center")
+        lab.set_horizontalalignment("center")
+        lab.set_rotation_mode("anchor")
+
+    for z, (label, angle) in enumerate(zip(ax.get_yticklabels(), a)):
+        x, y = label.get_position()
+        print(label, angle)
+        lab = ax.text(
+            x,
+            y,
+            label.get_text(),
+            transform=label.get_transform(),
+            fontsize=4,
+            c=(0.7, 0.7, 0.7),
+        )
+        lab.set_rotation(-(angles[0] + angles[1]) / 2)
+
+        lab.set_verticalalignment("bottom")
+        lab.set_horizontalalignment("center")
+        lab.set_rotation_mode("anchor")
+
+    ax.set_xticklabels([])
+    ax.set_yticklabels([])
+
+    plt.savefig("plots/experiment21/radars/%s/%s_%s.png" % (what, classifier_name, parameter_name), bbox_inches='tight', dpi=250)
+    plt.close()
+
+for j, clf in enumerate(clfs):
+    print("\n###\n### %s\n###\n" % (clf))
+    for i, metric in enumerate(metrics):
+        print("\n---\n--- %s\n---\n" % (metric))
+        # KLASYFIKATOR, CHUJ, DIST, LABELNOISE, METHOD, CHUNK, METRYKA
+        sub_scores = scores[j, :, :, :, :, :, i]
+        # print(sub_scores.shape)
+
+        # LABNO
+        # CHUJ, DIST, LABELNOISE, METHOD, CHUNK
+        reduced_scores = np.mean(sub_scores, axis=0)
+        reduced_scores = np.mean(reduced_scores, axis=0)
+        table = []
+        header = ["LN"] + methods
+        for k, label_noise in enumerate(label_noises):
+            # LABELNOISE, METHOD, CHUNK
+            selected_scores = reduced_scores[k]
+            mean_scores = np.mean(selected_scores, axis=1)
+            table.append([label_noise] + ["%.3f" % score for score in mean_scores])
+
+            plot_runs(clfs, metrics, selected_scores, methods, mean_scores, ln, "label_noise")
+
+        print(tabulate(table, headers=header))
+        print("")
+
+        # DISTRIBUTION
+        # CHUJ, DIST, LABELNOISE, METHOD, CHUNK
+        reduced_scores = np.mean(sub_scores, axis=0)
+        reduced_scores = np.mean(reduced_scores, axis=1)
+        table = []
+        header = ["Dist"] + methods
+        for k, distribution in enumerate(distributions):
+            # LABELNOISE, METHOD, CHUNK
+            selected_scores = reduced_scores[k]
+            mean_scores = np.mean(selected_scores, axis=1)
+            table.append([distribution] + ["%.3f" % score for score in mean_scores])
+
+            plot_runs(clfs, metrics, selected_scores, methods, mean_scores, dist, "distributions")
+
+        # print(table)
+        print(tabulate(table, headers=header))
+        print("")
+
+        # Drift
+        # CHUJ, DIST, LABELNOISE, METHOD, CHUNK
+        reduced_scores = np.mean(sub_scores, axis=1)
+        reduced_scores = np.mean(reduced_scores, axis=1)
+        table = []
+        header = ["Drift"] + methods
+        for k, drift in enumerate(drifts):
+            # LABELNOISE, METHOD, CHUNK
+            selected_scores = reduced_scores[k]
+            mean_scores = np.mean(selected_scores, axis=1)
+            table.append([drift] + ["%.3f" % score for score in mean_scores])
+
+            plot_runs(clfs, metrics, selected_scores, methods, mean_scores, drifts, "drift_type")
+
+        # print(table)
+        print(tabulate(table, headers=header))
+        print("")
+
+# RADAR DIAGRAMS
+
+for j, clf in enumerate(clfs):
+    print("\n###\n### %s\n###\n" % (clf))
+    for i, drift in enumerate(drifts):
+        print("\n---\n--- %s\n---\n" % (drift))
+        # KLASYFIKATOR, CHUJ, DIST, LABELNOISE, METHOD, CHUNK, METRYKA
+        sub_scores = scores[j, i, :, :, :, :, :]
+
+        # Metryka
+        # DIST, LABELNOISE, METHOD, CHUNK, METRYKA
+        reduced_scores = np.mean(sub_scores, axis=0)
+        reduced_scores = np.mean(reduced_scores, axis=0)
+        table = []
+        header = ["Metric"] + methods
+        for k, metric in enumerate(metrics):
+            # METHOD, CHUNK, Metryka
+            selected_scores = reduced_scores[:,:,k]
+            mean_scores = np.mean(selected_scores, axis=1)
+            table.append([metric] + ["%.3f" % score for score in mean_scores])
+
+        # print(table)
+        print(tabulate(table, headers=header))
+        print("")
+
+        plot_radars(methods, metrics, table, clf, drift, "drift_type")
+
+    for i, distribution in enumerate(distributions):
+        print("\n---\n--- %s\n---\n" % (distribution))
+        # KLASYFIKATOR, CHUJ, DIST, LABELNOISE, METHOD, CHUNK, METRYKA
+        sub_scores = scores[j, :, i, :, :, :, :]
+
+        # Metryka
+        # CHUJ, LABELNOISE, METHOD, CHUNK, METRYKA
+        reduced_scores = np.mean(sub_scores, axis=0)
+        reduced_scores = np.mean(reduced_scores, axis=0)
+        table = []
+        header = ["Metric"] + methods
+        for k, metric in enumerate(metrics):
+            # METHOD, CHUNK, Metryka
+            selected_scores = reduced_scores[:,:,k]
+            mean_scores = np.mean(selected_scores, axis=1)
+            table.append([metric] + ["%.3f" % score for score in mean_scores])
+
+        # print(table)
+        print(tabulate(table, headers=header))
+        print("")
+
+        plot_radars(methods, metrics, table, clf, distribution, "distributions")
+
+    for i, label_noise in enumerate(label_noises):
+        print("\n---\n--- %s\n---\n" % (label_noise))
+        # KLASYFIKATOR, CHUJ, DIST, LABELNOISE, METHOD, CHUNK, METRYKA
+        sub_scores = scores[j, :, :, i, :, :, :]
+
+        # Metryka
+        # CHUJ, LABELNOISE, METHOD, CHUNK, METRYKA
+        reduced_scores = np.mean(sub_scores, axis=0)
+        reduced_scores = np.mean(reduced_scores, axis=0)
+        table = []
+        header = ["Metric"] + methods
+        for k, metric in enumerate(metrics):
+            # METHOD, CHUNK, Metryka
+            selected_scores = reduced_scores[:,:,k]
+            mean_scores = np.mean(selected_scores, axis=1)
+            table.append([metric] + ["%.3f" % score for score in mean_scores])
+
+        # print(table)
+        print(tabulate(table, headers=header))
+        print("")
+
+        plot_radars(methods, metrics, table, clf, label_noise, "label_noise")
---
+++ gather_21.py
@@ -0,0 +1,70 @@
+from strlearn.streams import StreamGenerator
+import numpy as np
+
+# Variables
+clfs = ["GNB"]
+methods = ["NON-KNORAU2", "ROS-KNORAU2", "B2-KNORAU2", "NON-KNORAE2", "ROS-KNORAE2", "B2-KNORAE2"]
+random_states = [1994, 1410]
+distributions = [[0.95, 0.05], [0.9, 0.1]]
+label_noises = [
+    0.01,
+    0.03,
+    0.05,
+]
+
+chuj = [(5, False), (5, True), (None, False)]
+
+n_chunks = 199
+metrics = ["BAC", "geometric_mean_score", "f_score", "precision", "recall", "specificity"]
+
+scores = np.zeros(
+    (
+        len(clfs),
+        len(random_states),
+        len(chuj),
+        # len(incremental),
+        len(distributions),
+        len(label_noises),
+        # len(css),
+        len(methods),
+        n_chunks,
+        len(metrics),
+    )
+)
+
+print(scores.shape)
+
+# Prepare streams
+streams = {}
+for i, clf in enumerate(clfs):
+    for j, random_state in enumerate(random_states):
+        for k, kurwa in enumerate(chuj):
+            for l, distribution in enumerate(distributions):
+                for m, flip_y in enumerate(label_noises):
+                    # for n, spacing in enumerate(css):
+                    spacing, drift_type = kurwa
+                    stream = StreamGenerator(
+                        incremental=drift_type,
+                        weights=distribution,
+                        random_state=random_state,
+                        y_flip=flip_y,
+                        concept_sigmoid_spacing=spacing,
+                        n_drifts=1,
+                        n_chunks=200,
+                        chunk_size=250,
+                        n_clusters_per_class = 1,
+                        n_features = 8,
+                        n_informative= 8,
+                        n_redundant= 0,
+                        n_repeated = 0,
+                    )
+                    if spacing == None and drift_type == True:
+                        pass
+                    else:
+                        results = np.load(
+                            "results/experiment21_%s/%s.npy" % (clf, stream)
+                        )
+                        scores[i, j, k, l, m] = results
+scores = np.mean(scores, axis=1)
+np.save("scores_21", scores)
+print(scores, scores.shape)
